653b3f8ef97748e8a8c9ca881e4538e8d76c93e0
GoodnessEzeokafor <gootech442@gmail.com>
2022-11-08 22:38:08 +0100

    fix: updated changes

--- src/services/use-cases/trade/p2p/p2p.service.ts
+++ src/services/use-cases/trade/p2p/p2p.service.ts
@@ -439,15 +439,15 @@ export class P2pServices {
   async createAdsBank(payload: ICreateP2pAdBank) {
     try {
       const { accountNumber, userId } = payload
-      const bankExists = await this.data.p2pAdBanks.findOne({ userId, accountNumber })
-      if (bankExists) {
-        return Promise.reject({
-          status: HttpStatus.CONFLICT,
-          state: ResponseState.ERROR,
-          message: 'Bank already exists',
-          error: null
-        })
-      }
+      // const bankExists = await this.data.p2pAdBanks.findOne({ userId, accountNumber })
+      // if (bankExists) {
+      //   return Promise.reject({
+      //     status: HttpStatus.CONFLICT,
+      //     state: ResponseState.ERROR,
+      //     message: 'Bank already exists',
+      //     error: null
+      //   })
+      // }
       const factory = await this.p2pAdsBankFactory.create(payload)
       const data = await this.data.p2pAdBanks.create(factory)
 
