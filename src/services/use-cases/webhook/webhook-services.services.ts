84605cf7587aa30fcb28bb764b9f8e879192aa62
GoodnessEzeokafor <gootech442@gmail.com>
2022-11-20 03:28:24 +0100

    fix: transaction description

--- src/services/use-cases/webhook/webhook-services.services.ts
+++ src/services/use-cases/webhook/webhook-services.services.ts
@@ -61,7 +61,7 @@ export class WebhookServices {
         this.data.transactions.findOne({ tatumTransactionId: txId }),
         this.data.transactions.findOne({ hash: blockHash })
       ])
-      if (!wallet){
+      if (!wallet) {
         await this.discord.inHouseNotification({
           title: `Incoming Deposit V.1 :- ${env.env} environment`,
           message: `
@@ -74,7 +74,7 @@ export class WebhookServices {
 
         return Promise.resolve({ message: 'Wallet does not exists' })
       }
-      if (referenceAlreadyExists){
+      if (referenceAlreadyExists) {
         await this.discord.inHouseNotification({
           title: `Incoming Deposit V.1 :- ${env.env} environment`,
           message: `
@@ -86,8 +86,8 @@ export class WebhookServices {
         })
 
         return Promise.resolve({ message: 'reference already exists' })
-      } 
-      if (transactionIdAlreadyExists){
+      }
+      if (transactionIdAlreadyExists) {
         await this.discord.inHouseNotification({
           title: `Incoming Deposit V.1 :- ${env.env} environment`,
           message: `
@@ -100,8 +100,8 @@ export class WebhookServices {
 
         return Promise.resolve({ message: 'tatumTransactionId already exists' })
       }
-      if (transactionHashAlreadyExists){
-  await this.discord.inHouseNotification({
+      if (transactionHashAlreadyExists) {
+        await this.discord.inHouseNotification({
           title: `Incoming Deposit V.1 :- ${env.env} environment`,
           message: `
           Transaction hash already exists
@@ -232,7 +232,7 @@ export class WebhookServices {
         ,
 
       ])
-      if (!account){
+      if (!account) {
         await this.discord.inHouseNotification({
           title: `Incoming Deposit :- ${env.env} environment`,
           message: `
@@ -243,8 +243,8 @@ export class WebhookServices {
           link: env.isProd ? EXTERNAL_DEPOSIT_CHANNEL_LINK_PRODUCTION : EXTERNAL_DEPOSIT_CHANNEL_LINK,
         })
         return Promise.resolve({ message: 'Wallet does not exists' })
-      } 
-      if (referenceAlreadyExists){
+      }
+      if (referenceAlreadyExists) {
         await this.discord.inHouseNotification({
           title: `Incoming Deposit :- ${env.env} environment`,
           message: `
@@ -255,8 +255,8 @@ export class WebhookServices {
           link: env.isProd ? EXTERNAL_DEPOSIT_CHANNEL_LINK_PRODUCTION : EXTERNAL_DEPOSIT_CHANNEL_LINK,
         })
         return Promise.resolve({ message: 'reference already exists' })
-      } 
-      if (transactionIdAlreadyExists){
+      }
+      if (transactionIdAlreadyExists) {
         await this.discord.inHouseNotification({
           title: `Incoming Deposit :- ${env.env} environment`,
           message: `
@@ -267,8 +267,8 @@ export class WebhookServices {
           link: env.isProd ? EXTERNAL_DEPOSIT_CHANNEL_LINK_PRODUCTION : EXTERNAL_DEPOSIT_CHANNEL_LINK,
         })
         return Promise.resolve({ message: 'tatumTransactionId already exists' })
-      } 
-      if (transactionHashAlreadyExists){
+      }
+      if (transactionHashAlreadyExists) {
         await this.discord.inHouseNotification({
           title: `Incoming Deposit :- ${env.env} environment`,
           message: `
@@ -279,7 +279,7 @@ export class WebhookServices {
           link: env.isProd ? EXTERNAL_DEPOSIT_CHANNEL_LINK_PRODUCTION : EXTERNAL_DEPOSIT_CHANNEL_LINK,
         })
         return Promise.resolve({ message: 'Transaction hash already exists' })
-      } 
+      }
 
       const user = await this.data.users.findOne({ _id: account.userId })
       const depositAddress = await this.data.depositAddresses.findOne({ address: to, coin: currency.toUpperCase(), userId: account.userId })
@@ -505,7 +505,7 @@ export class WebhookServices {
         this.data.virtualAccounts.findOne({ accountId, coin: currency.toUpperCase() })
       ])
 
-      if (transactionAlreadyExists){
+      if (transactionAlreadyExists) {
         await this.discord.inHouseNotification({
           title: `Withdrawal :- ${env.env} environment`,
           message: `
@@ -517,8 +517,8 @@ export class WebhookServices {
         })
 
         return Promise.resolve({ message: 'Transaction already exists' })
-      } 
-      if (!transaction){
+      }
+      if (!transaction) {
         await this.discord.inHouseNotification({
           title: `Withdrawal :- ${env.env} environment`,
           message: `
@@ -529,8 +529,8 @@ export class WebhookServices {
           link: env.isProd ? EXTERNAL_DEPOSIT_CHANNEL_LINK_PRODUCTION : EXTERNAL_DEPOSIT_CHANNEL_LINK,
         })
         return Promise.resolve({ message: 'Transaction does not exists or already completed' })
-      } 
-      if (!virtualAccount){
+      }
+      if (!virtualAccount) {
         await this.discord.inHouseNotification({
           title: `Withdrawal :- ${env.env} environment`,
           message: `
@@ -578,6 +578,7 @@ export class WebhookServices {
             { _id: transaction._id },
             {
               status: Status.COMPLETED,
+              description: 'Withdrawal successful',
               reference,
               balanceAfter: deductedAccount.balance,
               metadata: JSON.stringify(payload)
