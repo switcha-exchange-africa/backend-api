469547e61fbb21b637facafc755860af7b07245d
GoodnessEzeokafor <gootech442@gmail.com>
2022-11-24 12:35:44 +0100

    feat: web platform

--- src/middleware-guards/logs.middleware.ts
+++ src/middleware-guards/logs.middleware.ts
@@ -17,6 +17,27 @@ export enum ActionType {
   DELETE = "delete",
   PATCH = "patch"
 }
+enum Platform {
+  WEB = 'web',
+  ANDROID = 'android',
+  IOS = 'ios'
+}
+enum Operation {
+  SIGNUP = 'signup',
+  SIGNIN = 'signin',
+  VERIFY_EMAIL = 'verify-email',
+  WITHDRAWAL = 'withdrawal',
+  DEPOSIT = 'deposit',
+  SWAP = 'swap',
+  BUY = 'buy',
+  SELL = 'sell',
+  P2P_SELL = 'p2p-sell',
+  P2P_BUY = 'p2p-buy',
+  QUICK_TRADE_BUY = 'quick-trade-buy',
+  QUICK_TRADE_SELL = 'quick-trade-sell',
+  P2P_SELL_AD = 'p2p-sell-ad',
+  P2P_BUY_AD = 'p2p-buy-ad',
+}
 
 const returnState = (code: number) => {
   if (code >= 500) return State.ERROR;
@@ -64,9 +85,9 @@ class LogsMiddleware implements NestMiddleware {
           originalUrl,
           statusCode: String(statusCode),
           sourceIpCountryCode,
-          platform: platform || 'not-set',
+          platform: platform as Platform || Platform.WEB,
           logMessage: '',
-          operation: operation || 'not-set',
+          operation: operation as Operation || 'not-set',
           operationId: '',
           userEmail: user ? user.email : null,
           app: 'switcha'
