b7c7d224adb14a007f4078f27268bf72ca4ecfe3
GoodnessEzeokafor <gootech442@gmail.com>
2023-01-07 16:34:20 +0100

    fix: tracking

--- src/controllers/home/index.ts
+++ src/controllers/home/index.ts
@@ -4,16 +4,16 @@ import { HomeServices } from 'src/services/use-cases/home/home.service';
 var webPush = require('web-push');
 
 webPush.setVapidDetails(
-	FRONTEND_URL,
-	WEB_PUSH_PUBLIC_KEY,
-	WEB_PUSH_PRIVATE_KEY
-  );
+  FRONTEND_URL,
+  WEB_PUSH_PUBLIC_KEY,
+  WEB_PUSH_PRIVATE_KEY
+);
 
 
-  
-  
-  
-  
+
+
+
+
 @Controller()
 export class HomeController {
   constructor(
@@ -27,7 +27,7 @@ export class HomeController {
 
   @Get('/name')
   name(): string {
-    return `${env.env.toUpperCase()} API`;
+    return `${env.env.toUpperCase()} UPDATE TRACKING API`;
   }
 
 
